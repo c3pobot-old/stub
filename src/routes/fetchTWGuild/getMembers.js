33b0c0c080250dee9bf395c86f76b0017cfc8264
scuba <scuba75@gmail.com>
2023-07-31 19:40:10 +0000

    update

--- src/routes/fetchTWGuild/getMembers.js
+++ src/routes/fetchTWGuild/getMembers.js
@@ -29,7 +29,7 @@ const getNewPlayers = async(missingMembers = [])=>{
 module.exports = async(guildId, collection = 'twPlayerCache', member = [], projection)=>{
   try{
     let foundMemberIds = [], memberIds = member.map(x=>x.playerId)
-    let res = await getCachePlayers(memberIds, projection)
+    let res = await getCachePlayers(memberIds, playerCache, projection)
     if(res?.length === member.length) return res
     if(res?.length > 0 ) foundMemberIds = res.map(x=>x.playerId)
     let missingMembers = member.filter(x=>!foundMemberIds.includes(x.playerId))
